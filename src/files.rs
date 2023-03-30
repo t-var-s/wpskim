184e0dd64b2fb5c0f2650303ea3524457f661ac8
Ricardo Tavares <ricardo.tavares@viewfromtheweb.com>
2023-03-30 23:04:00 +0100

    fs::read_to_string(path).ok()

--- src/files.rs
+++ src/files.rs
@@ -1,19 +1,11 @@
-use std::fs::File;
-use std::io::Read;
+use std::fs;
 use std::io::Write;
 
 pub fn load_text(path: &str) -> Option<String> {
-    let mut text = String::new();
-    match File::open(&path) {
-        Ok(mut file) => match file.read_to_string(&mut text) {
-            Ok(_) => Some(text),
-            Err(_) => None,
-        },
-        Err(_) => None,
-    }
+    fs::read_to_string(path).ok()
 }
 pub fn save_text(path: &str, json_text: &str) -> () {
-    match File::create(path) {
+    match fs::File::create(path) {
         Ok(mut file) => {
             match file.write_all(&json_text.to_string().into_bytes()) {
                 Ok(_) => (),
